4ac53f65abe30c7e99eddf1508770038c37cf7fe
jamshidjanibekov <jamshidjanibekov0@gmail.com>
2024-09-09 23:08:08 +0500

    Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButton, MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatToolbarModule, MatIconButton, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

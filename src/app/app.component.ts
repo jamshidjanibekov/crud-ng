92d8c521568f2352698f72fc4dc80ca65383022b
jamshidjanibekov <jamshidjanibekov0@gmail.com>
2024-09-10 10:59:18 +0500

    dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatDialogModule} from '@angular/material/dialog';
+import {DialogComponent} from "./dialog/dialog.component";
+import {MatDialog} from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, MatIcon, MatToolbarModule, MatIconButton, MatButton],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatToolbarModule, MatIconButton, MatButton, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  dialog = inject(MatDialog);
   title = 'ng-app';
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width:'30%'
+    });
+
+  }
 }
